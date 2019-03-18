349f60400687508add07a96552530187d996473d
Tomasz Rusin <tomek@rusin.info>
2019-03-18 11:10:04 +0100

    Add scss for contacts

---
+++ components/Contact.js
@@ -0,0 +1,22 @@
+
+var Contact = React.createClass({
+    propTypes: {
+      item: React.PropTypes.object.isRequired,
+    },
+  
+    render: function() {
+      return (
+        React.createElement('div', {className: 'contactItem'},
+          React.createElement('img', {
+            className: 'contactImage',
+            src: 'https://www.sbp-creative.com/wp-content/uploads/2017/01/profile-placeholder.png'
+          }),
+          React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
+          React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
+          React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email},
+            this.props.item.email
+          )
+        )
+      )
+    },
+  });
